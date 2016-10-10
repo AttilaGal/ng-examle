7d206b7e11e5645227f23bcf407d117ef44e6438
Attila Gal <attila.gal@gmail.com>
2016-10-10 21:36:44 +0200

    added component callback demo

---
+++ component-callback/color-panel/color-selector/color-selector.component.js
@@ -0,0 +1,23 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('color-panel')
+        .component('colorSelector', {
+            bindings: {
+                colors: '<',
+                callback: '&'
+            },
+            templateUrl: './color-panel/color-selector/color-selector.template.html',
+            controller: ColorSelector
+        });
+
+    function ColorSelector(){
+        var vm = this;
+
+        vm.colorsChanged = function(){
+            vm.callback({colorArray: vm.colors})
+        }
+    }
+
+})();
