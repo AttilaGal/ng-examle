1e2f8a0daecfdb3942dd59eceabfc7405af82637
Attila Gal <attila.gal@gmail.com>
2016-11-14 22:14:29 +0100

    controller demo finalized

---
+++ controllers/simpleController.js
@@ -0,0 +1,15 @@
+(function () {
+  "use strict";
+  
+  angular.module('ControllerExample')
+    .controller('simpleController', SimpleController);
+  
+  function SimpleController() {
+    //logic comes here
+    var vm = this;
+    
+    vm.clickHandler = function(){
+      alert('Controller is working!');
+    }
+  }
+})();
